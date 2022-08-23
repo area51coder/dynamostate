aa5ccb00e554c3fc4737a2208d6d8074f3b99f15
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-23 12:34:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
 backend "s3" {
 # Replace this with your bucket name!
 bucket = "terraform-up-and-running-state-area51"
-key = "global/s3/terraform.tfstate"
+key = "s3/terraform.tfstate"
 region = "us-east-2"
 # Replace this with your DynamoDB table name!
 dynamodb_table = "terraform-up-and-running-locks"
